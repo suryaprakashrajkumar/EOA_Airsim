3a0376d7fc233ae744a33edfaa6e6762c9d9f9ef
SURYAPRAKASHRH <63646497+SURYAPRAKASHRH@users.noreply.github.com>
2022-02-23 15:00:26 +0530

    main

--- airsim_api.py
+++ airsim_api.py
@@ -1,6 +1,7 @@
 
 #airsim_api
 #helper functions for airsims
+from matplotlib.pyplot import flag
 import airsim
 import numpy as np
 import time
@@ -12,7 +13,7 @@ height = 5
 client = airsim.MultirotorClient()
 client.confirmConnection()
 client.enableApiControl(True)
-
+flag = 0
 print (" ######################## UAV in start position ########################")
 
 print (" ################################# ARM #################################")
@@ -21,7 +22,7 @@ client.armDisarm(True) # False to disarm
 print (" ############################### TAKEOFF ###############################")
 client.takeoffAsync(timeout_sec=10)
 client.moveToZAsync( -2, 1).join()
-
+prev_wp = [0,0]
 
 print (" ###################### Avoidance Algorithm Start ######################")
 def process(score):
@@ -52,8 +53,18 @@ def direction(direction):
         wp[0] = wp[0] 
     elif direction == 3:
         wp[1] = wp[1]
-        wp[0] = wp[0] 
-    client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(wp[0], wp[1], -2), airsim.to_quaternion(0, 0, 0)), True)
+        wp[0] = wp[0]
+    
+    if flag==0:
+        client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(wp[0], wp[1], -2), airsim.to_quaternion(0, 0, 0)), True)
+        prev_wp = wp
+        flag = 1
+    else:
+        if wp[0] != prev_wp[0] and wp[1] != prev_wp[1]:
+            client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(wp[0], wp[1], -2), airsim.to_quaternion(0, 0, 0)), True)
+            prev_wp = wp
+        else:
+            print("holding")
     print(wp)
 
 def score(x1,x2,x3):
