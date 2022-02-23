ef8d4ad4fd6731d9f0d93d21a44f1d39f8faf6c1
SURYAPRAKASHRH <63646497+SURYAPRAKASHRH@users.noreply.github.com>
2022-02-23 15:42:42 +0530

    main

--- airsim_api.py
+++ airsim_api.py
@@ -1,7 +1,6 @@
 
 #airsim_api
 #helper functions for airsims
-from matplotlib.pyplot import flag
 import airsim
 import numpy as np
 import time
@@ -13,7 +12,7 @@ height = 5
 client = airsim.MultirotorClient()
 client.confirmConnection()
 client.enableApiControl(True)
-flag = 0
+
 print (" ######################## UAV in start position ########################")
 
 print (" ################################# ARM #################################")
@@ -22,7 +21,7 @@ client.armDisarm(True) # False to disarm
 print (" ############################### TAKEOFF ###############################")
 client.takeoffAsync(timeout_sec=10)
 client.moveToZAsync( -2, 1).join()
-prev_wp = [0,0]
+
 
 print (" ###################### Avoidance Algorithm Start ######################")
 def process(score):
@@ -42,11 +41,6 @@ def direction(direction):
     '''
     convert the direction to the corresponding action
     '''
-    global flag
-    global wp
-    global step
-    global prev_wp
-    print("Directions: ", direction)
     if direction == 1:
         wp[1] = wp[1]
         wp[0] = wp[0] + step
@@ -59,19 +53,7 @@ def direction(direction):
     elif direction == 3:
         wp[1] = wp[1]
         wp[0] = wp[0]
-    
-    if flag==0:
-        client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(wp[0], wp[1], -2), airsim.to_quaternion(0, 0, 0)), True)
-        prev_wp = wp
-        flag = 1
-    else:
-        if wp[0] != prev_wp[0] and wp[1] != prev_wp[1]:
-            print("prev wp: ", prev_wp)
-            client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(wp[0], wp[1], -2), airsim.to_quaternion(0, 0, 0)), True)
-            prev_wp = wp
-        else:
-            print("prev wp: ", prev_wp)
-            print("holding")
+    client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(wp[0], wp[1], -2), airsim.to_quaternion(0, 0, 0)), True)
     print(wp)
 
 def score(x1,x2,x3):
